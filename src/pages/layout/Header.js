df340cdde78dcee22968a25d446105417732b918
Prashmin Gurung <prashmingrg@gmail.com>
2023-08-18 21:22:00 +1000

    header section is in progress

--- src/pages/layout/Header.js
+++ src/pages/layout/Header.js
@@ -1,9 +1,11 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import { Link } from "react-router-dom";
+import Box from "@mui/material/Box";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
 import { useDispatch, useSelector } from "react-redux";
 import { requestSuccess } from "../login/authSlice";
+import { IconButton, Typography, Button } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import logo from "./images/Logo.jpg";
 
 export const Header = () => {
   const dispatch = useDispatch();
@@ -16,44 +18,15 @@ export const Header = () => {
     dispatch(requestSuccess({}));
   };
   return (
-    <Navbar bg="success" expand="md">
-      <Container>
-        <Navbar.Brand href="/">Admin-CMS</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            {user?._id ? (
-              <>
-                <Link to="/register" className="nav-link">
-                  <i class="fa-solid fa-bell" title="Notifications"></i>
-                </Link>
-                <Link to="/register" className="nav-link">
-                  <i class="fa-solid fa-user-pen" title="User Profile"></i>
-                </Link>
-
-                <Link to="/" className="nav-link" onClick={handleOnLogout}>
-                  <i class="fa-solid fa-right-to-bracket" title="Log out"></i>
-                </Link>
-              </>
-            ) : (
-              <>
-                <Link to="/" className="nav-link">
-                  {" "}
-                  <i
-                    className="fa-solid fa-right-to-bracket"
-                    title="User Profile"
-                  ></i>
-                  Login
-                </Link>
-                <Link to="/register" className="nav-link">
-                  {" "}
-                  <i className="fa-solid fa-user" title="LogOuts"></i>Sign Up{" "}
-                </Link>
-              </>
-            )}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <Box>
+      <AppBar position="static" sx={{ backgroundColor: "#fafafa" }}>
+        <Toolbar sx={{ display: "flex", flexDirection: "row-reverse" }}>
+          <img src={logo} alt="logo" style={{ height: "40px" }} />
+          <Button variant="contained" size="medium">
+            GET STARTED
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
