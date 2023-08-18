f8c4ef6ef72974ccc0fd1856c0a5d44b93734d7b
Prashmin Gurung <prashmingrg@gmail.com>
2023-08-18 18:09:47 +1000

    header is changed

--- src/pages/layout/Header.js
+++ src/pages/layout/Header.js
@@ -16,37 +16,38 @@ export const Header = () => {
     dispatch(requestSuccess({}));
   };
   return (
-    <Navbar bg="warning" expand="md">
+    <Navbar bg="success" expand="md">
       <Container>
-        <Link to="/" className="navbar-brand">
-          Admin CMS
-        </Link>
+        <Navbar.Brand href="/">Admin-CMS</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             {user?._id ? (
               <>
                 <Link to="/register" className="nav-link">
-                  <i className="fa-solid fa-bell" title="Notifications"></i>
+                  <i class="fa-solid fa-bell" title="Notifications"></i>
                 </Link>
                 <Link to="/register" className="nav-link">
-                  <i className="fa-solid fa-user-pen" title="User Profile"></i>
+                  <i class="fa-solid fa-user-pen" title="User Profile"></i>
                 </Link>
 
                 <Link to="/" className="nav-link" onClick={handleOnLogout}>
-                  <i
-                    className="fa-solid fa-right-to-bracket"
-                    title="Log out"
-                  ></i>
+                  <i class="fa-solid fa-right-to-bracket" title="Log out"></i>
                 </Link>
               </>
             ) : (
               <>
                 <Link to="/" className="nav-link">
-                  <i className="fa-solid fa-right-to-bracket"></i> Login
+                  {" "}
+                  <i
+                    className="fa-solid fa-right-to-bracket"
+                    title="User Profile"
+                  ></i>
+                  Login
                 </Link>
                 <Link to="/register" className="nav-link">
-                  <i className="fa-solid fa-user-pen"></i> Sign Up
+                  {" "}
+                  <i className="fa-solid fa-user" title="LogOuts"></i>Sign Up{" "}
                 </Link>
               </>
             )}
